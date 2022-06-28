b281df9b0c33ebbc5ce3b1cb85f160aaed8c63a0
Kevin Genchev <kevin_genchev@epam.com>
2022-06-28 09:28:40 +0300

    Prepare for deploy 4

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/tickets", require("./routes/ticketRoutes"));
 // Serve Frondend
 if (process.env.NODE_ENV === "production") {
   // Set build folder as static
-  app.use(express.static(path.join(__dirname, "../frondend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
     res.sendFile(__dirname, "../", "frontend", "build", "index.html")
